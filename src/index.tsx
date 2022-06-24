cfa7bc4bb3b40fd541489a039d9ba9cf12eac472
YaroslavChuiko <32570823+JARIK0803@users.noreply.github.com>
2022-06-24 22:15:06 +0300

    add routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,16 @@
-import React from "react";
+import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './components/App';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './styles/index.scss';
 
 const container = document.getElementById('app');
 const root = createRoot(container);
 
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
