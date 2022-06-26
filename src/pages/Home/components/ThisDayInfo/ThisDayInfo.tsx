3755cc9f96c524f03fb2b6260a3cc86079510ea9
YaroslavChuiko <32570823+JARIK0803@users.noreply.github.com>
2022-06-26 15:23:21 +0300

    add ThisDayInfo component

--- src/pages/Home/components/ThisDayInfo/ThisDayInfo.tsx
+++ src/pages/Home/components/ThisDayInfo/ThisDayInfo.tsx
@@ -1,13 +1,44 @@
-import React from 'react'
+import React from 'react';
+import DailyDetailsGrid from '../../../../shared/DailyDetailsGrid/DailyDetailsGrid';
 
 import s from './ThisDayInfo.module.scss';
 
-type Props = {}
+import temp from '../../../../assets/icons/temp.svg?url';
+import pressure from '../../../../assets/icons/pressure.svg?url';
+import precipitation from '../../../../assets/icons/precipitation.svg?url';
+import wind from '../../../../assets/icons/wind.svg?url';
+
+type Props = {};
 
 const ThisDayInfo = (props: Props) => {
-  return (
-    <div className={s.home}>Home</div>
-  )
-}
+  const items = [
+    {
+      icon_url: temp,
+      name: 'Температура',
+      value: '20° - ощущается как 17°',
+    },
+    {
+      icon_url: pressure,
+      name: 'Давление',
+      value: '765 мм ртутного столба - нормальное',
+    },
+    {
+      icon_url: precipitation,
+      name: 'Осадки',
+      value: 'Без осадков',
+    },
+    {
+      icon_url: wind,
+      name: 'Ветер',
+      value: '3 м/с юго-запад - легкий ветер',
+    },
+  ];
 
-export default ThisDayInfo
+  return (
+    <div className={s.this_day_info}>
+      <DailyDetailsGrid rows={items} />
+    </div>
+  );
+};
+
+export default ThisDayInfo;
