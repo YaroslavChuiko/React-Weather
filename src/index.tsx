1581f1fc77f637b1513c4ed7401b96b12a2b7146
YaroslavChuiko <32570823+JARIK0803@users.noreply.github.com>
2022-07-02 21:51:27 +0300

    add redux store and weather slice

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { ThemeProvider } from './provider/ThemeProvider';
+import { store } from './store/store';
 import './styles/index.scss';
 
 const container = document.getElementById('app');
@@ -10,10 +12,12 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
