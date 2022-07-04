591e4b92e880c85e958e384f1d30fc1eb42b5b2c
YaroslavChuiko <32570823+JARIK0803@users.noreply.github.com>
2022-07-04 15:53:08 +0300

    add city switcher

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { storage } from './model/Storage';
 import Home from './pages/Home/Home';
 import MonthStatistics from './pages/MonthStatistics/MonthStatistics';
 import Header from './shared/Header/Header';
 import { Popup } from './shared/Popup/Popup';
 
+export type Option = {
+  value: string;
+  label: string;
+};
+
 const App = () => {
+  const selectCityOptions: Option[] = [
+    { value: 'kyiv', label: 'Киев' },
+    { value: 'kharkiv', label: 'Харьков' },
+    { value: 'lviv', label: 'Львов' },
+  ];
+
+  const [city, setCity] = useState(storage.getItem('city') || selectCityOptions[0].value);
+
   return (
     <div className="container">
-      <Header />
+      <Header city={city} selectCityOptions={selectCityOptions} setCity={setCity} />
       <Popup />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home city={city} />} />
         <Route path="/month-statistics" element={<MonthStatistics />} />
       </Routes>
     </div>
--- src/shared/Header/Header.tsx
+++ src/shared/Header/Header.tsx
@@ -1,44 +1,48 @@
-import React, { useEffect, useState } from 'react';
-import Select from 'react-select';
+import React from 'react';
 import { useTheme } from '../../hooks/useTheme';
 import { Theme } from '../../context/ThemeContext';
 
 import s from './Header.module.scss';
 import LogoIcon from '../../assets/icons/logo.svg';
 import ChangeThemeIcon from '../../assets/icons/change-theme.svg';
+import { storage } from '../../model/Storage';
+import { Option } from '../../App';
 
-type Props = {};
+type Props = {
+  city: string;
+  selectCityOptions: Option[];
+  setCity: React.Dispatch<React.SetStateAction<string>>;
+};
 
 //TODO: add styles to react select
 
-const Header = (props: Props) => {
+const Header = ({ city, selectCityOptions, setCity }: Props) => {
   const theme = useTheme();
 
   const changeTheme = () => {
     theme.changeTheme(theme.theme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT);
   };
 
-  const options = [
-    { value: 'city-1', label: 'Киев' },
-    { value: 'city-2', label: 'Харьков' },
-    { value: 'city-3', label: 'Львов' },
-  ];
-
-  const colorStyles = {
-    control: (styles: any) => ({
-      ...styles,
-      backgroundColor: theme.theme === Theme.DARK ? '#4F4F4F' : 'rgba(71, 147, 255, 0.2)',
-      width: '194px',
-      height: '37px',
-      border: 'none',
-      borderRadius: '10px',
-      zIndex: 100,
-    }),
-    singleValue: (styles: any) => ({
-      ...styles,
-      color: theme.theme === Theme.DARK ? '#fff' : '#000',
-    }),
+  const handleChangeCity = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setCity(e.target.value);
+    storage.setItem('city', e.target.value);
   };
+
+  // const colorStyles = {
+  //   control: (styles: any) => ({
+  //     ...styles,
+  //     backgroundColor: theme.theme === Theme.DARK ? '#4F4F4F' : 'rgba(71, 147, 255, 0.2)',
+  //     width: '194px',
+  //     height: '37px',
+  //     border: 'none',
+  //     borderRadius: '10px',
+  //     zIndex: 100,
+  //   }),
+  //   singleValue: (styles: any) => ({
+  //     ...styles,
+  //     color: theme.theme === Theme.DARK ? '#fff' : '#000',
+  //   }),
+  // };
 
   return (
     <header className={s.header}>
@@ -52,7 +56,16 @@ const Header = (props: Props) => {
         <div className={s.change_theme} onClick={changeTheme}>
           <ChangeThemeIcon />
         </div>
-        <Select defaultValue={options[0]} styles={colorStyles} options={options} />
+
+        <select value={city} onChange={handleChangeCity}>
+          {selectCityOptions.map((item: Option) => {
+            return (
+              <option key={item.value} value={item.value}>
+                {item.label}
+              </option>
+            );
+          })}
+        </select>
       </div>
     </header>
   );
