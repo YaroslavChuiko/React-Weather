ef5b3e6a9b3191dd20c1bfb8777a39223c93a600
YaroslavChuiko <32570823+JARIK0803@users.noreply.github.com>
2022-07-09 15:02:37 +0300

    add sessionStorage model

--- src/model/Storage.ts
+++ src/model/Storage.ts
@@ -1,9 +1,22 @@
-export const storage = {
+export const lStorage = {
   setItem: (name: string, item: any) => {
     localStorage.setItem(name, JSON.stringify(item));
   },
   getItem: (name: string) => {
     const item = localStorage.getItem(name);
+
+    if (item) {
+      return JSON.parse(item);
+    }
+  },
+};
+
+export const sStorage = {
+  setItem: (name: string, item: any) => {
+    sessionStorage.setItem(name, JSON.stringify(item));
+  },
+  getItem: (name: string) => {
+    const item = sessionStorage.getItem(name);
 
     if (item) {
       return JSON.parse(item);
